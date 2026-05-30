da711a29ca44947dd35038bc0a5348274906370c
pheaktra1402 <pheaktrathorn474@email.com>
2026-05-30 17:14:57 +0700

    Fix Docker configuration for Laravel and Vue

--- vuejs-app/vite.config.js
+++ vuejs-app/vite.config.js
@@ -1,18 +1,23 @@
-import { fileURLToPath, URL } from 'node:url'
+import { fileURLToPath, URL } from "node:url";
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import vueDevTools from 'vite-plugin-vue-devtools'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import vueDevTools from "vite-plugin-vue-devtools";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-  ],
+  plugins: [vue(), vueDevTools()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
   },
-})
+  // ADD THIS SERVER BLOCK BELOW:
+  server: {
+    host: "0.0.0.0", // Forces Vite to listen to network traffic outside the container
+    port: 5173,
+    watch: {
+      usePolling: true, // Essential for saving changes to update instantly inside Docker
+    },
+  },
+});
