857ae75fd508c0f1eb404cf390ad74ec200020a9
pheaktra1402 <pheaktrathorn474@email.com>
2026-06-28 19:30:20 +0700

    session3.0

--- vuejs-app/vite.config.js
+++ vuejs-app/vite.config.js
@@ -1,23 +1,31 @@
-import { fileURLToPath, URL } from "node:url";
+import { fileURLToPath, URL } from 'node:url'
 
-import { defineConfig } from "vite";
-import vue from "@vitejs/plugin-vue";
-import vueDevTools from "vite-plugin-vue-devtools";
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueDevTools()],
+  plugins: [
+    vue(),
+    vueDevTools(),
+  ],
   resolve: {
     alias: {
-      "@": fileURLToPath(new URL("./src", import.meta.url)),
+      '@': fileURLToPath(new URL('./src', import.meta.url))
     },
   },
-  // ADD THIS SERVER BLOCK BELOW:
   server: {
-    host: "0.0.0.0", // Forces Vite to listen to network traffic outside the container
+    host: '0.0.0.0',
     port: 5173,
-    watch: {
-      usePolling: true, // Essential for saving changes to update instantly inside Docker
+    hmr: {
+      protocol: 'ws',
+      port: 5173,
     },
-  },
-});
+    watch: {
+      usePolling: true,
+      useFsEvents: true,
+      interval: 1000,
+    }
+  }
+})
